ff77d89aab1ba101dfc160308748ac97c07165da
hrithika2004 <144313849+hrithika2004@users.noreply.github.com>
2025-03-14 15:18:02 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "hi my name is HRITHIKAAA\n";
   return 0;
 }
